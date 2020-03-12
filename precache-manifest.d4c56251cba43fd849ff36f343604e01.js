e0db97e7353ae11e9d473927f5908c28b6cbbe0d
Wiktor Kukulski <laxytt@gmail.com>
2020-03-12 13:02:40 +0100

    Updates

---
+++ precache-manifest.d4c56251cba43fd849ff36f343604e01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a6e76c3002a1b57b7781dac492b39c0",
+    "url": "/simple-youtube-web/index.html"
+  },
+  {
+    "revision": "997c81abc8f363a6e23f",
+    "url": "/simple-youtube-web/static/css/main.03c6ca28.chunk.css"
+  },
+  {
+    "revision": "d089dcda1c4348c64c83",
+    "url": "/simple-youtube-web/static/js/2.c04c682e.chunk.js"
+  },
+  {
+    "revision": "59fc29711a22a223e9f602decc11b79f",
+    "url": "/simple-youtube-web/static/js/2.c04c682e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "997c81abc8f363a6e23f",
+    "url": "/simple-youtube-web/static/js/main.3b878c36.chunk.js"
+  },
+  {
+    "revision": "3eb2400fa9e3dda8725f",
+    "url": "/simple-youtube-web/static/js/runtime-main.0fb2bbd7.js"
+  }
+]);
